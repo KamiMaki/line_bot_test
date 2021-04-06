3d8f51a745c183016019d091bc26b3e0a6c0ce91
KamiMaki <41109762+KamiMaki@users.noreply.github.com>
2021-04-06 23:19:40 +0800

    new json

--- app.py
+++ app.py
@@ -37,19 +37,19 @@ def handle_message(event):
     
     with open('card.json',encoding='utf-8-sig', errors='ignore') as f:
         card = json.loads(f.read())
-    with open('news.json',encoding='utf-8-sig', errors='ignore') as f:
-        news = json.loads(f.read())
-    flex_message = FlexSendMessage('card',card)
-    news = FlexSendMessage('news',news)
+    with open('latest_news.json',encoding='utf-8-sig', errors='ignore') as f:
+        latest_news = json.loads(f.read())
+    card_message = FlexSendMessage('card',card)
+    latest_news_message = TemplateSendMessage('news',latest_news)
     
     quick_reply = TextSendMessage(text='可透過下方類別了解更多：',quick_reply=QuickReply(items=[QuickReplyButton(action=MessageAction(label="CIO每周觀點", text="CIO每周觀點")), QuickReplyButton(action=MessageAction(label="ESG投資", text="ESG投資")), QuickReplyButton(action=MessageAction(label="股票", text="股票")),QuickReplyButton(action=MessageAction(label="固定收益", text="固定收益")),QuickReplyButton(action=MessageAction(label="新興市場債券", text="新興市場債券")),QuickReplyButton(action=MessageAction(label="label6", text="text6")),QuickReplyButton(action=MessageAction(label="label7", text="text7")),QuickReplyButton(action=MessageAction(label="label8", text="text8")),QuickReplyButton(action=MessageAction(label="label9", text="text9")),QuickReplyButton(action=MessageAction(label="label10", text="text10"))]))
     if event.message.text == '圖卡':
-        line_bot_api.reply_message(event.reply_token,flex_message)
+        line_bot_api.reply_message(event.reply_token,card_message)
     elif event.message.text == '觀點':
-        line_bot_api.reply_message(event.reply_token,news)
+        line_bot_api.reply_message(event.reply_token,latest_news_message)
     else:
         # line_bot_api.reply_message(event.reply_token,flex_message)
-        line_bot_api.reply_message(event.reply_token,[flex_message,quick_reply])
+        line_bot_api.reply_message(event.reply_token,[card_message,quick_reply])
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
